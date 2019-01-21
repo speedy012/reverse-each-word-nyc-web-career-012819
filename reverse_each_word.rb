4b28eaa761b58a0c066338d61c047a15ad3b3751
Herman <hrivas5726@aol.com>
2019-01-21 04:56:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
- def reverse_each_word(sentence1)
-   sentence1.each do { |sentence| print sentence.reverse_each_word} 
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
      
